218b1cbaf234c40a6f313ab972f05514cf39fb1e
jaideepr97 <jaideep.r97@gmail.com>
2018-03-29 11:39:07 +0530

    added bin file generator + movie_reviews_data_text

--- sample_vectors_generator.py
+++ sample_vectors_generator.py
@@ -1,6 +1,6 @@
 import fasttext
 
 # Skipgram model
-cbow = fasttext.cbow('test-pos.txt', 'sample_vectors')
+cbow = fasttext.cbow('movie_reviews_data_text/test-pos.txt', 'sample_vectors')
 print (cbow.words) # list of words in dictionary
 
